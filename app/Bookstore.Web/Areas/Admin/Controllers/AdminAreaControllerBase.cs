575f7cc58488c4a2ff1124987a3474b0f11487ea
Mark Fawaz <markfawa@amazon.com>
2024-07-08 14:08:10 -0700

    Remove areas

--- app/Bookstore.Web/Areas/Admin/Controllers/AdminAreaControllerBase.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using System.Web.Mvc;
-
-namespace Bookstore.Web.Areas.Admin.Controllers
-{
-    [RouteArea("Admin")]
-    [Authorize(Roles = "Administrators")]
-    public abstract class AdminAreaControllerBase : Controller { }
-}
